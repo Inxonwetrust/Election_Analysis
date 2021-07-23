2cd12cb512a26a4b9d4611f2d4f3b016e90b1971
Inxonwetrust <85142929+Inxonwetrust@users.noreply.github.com>
2021-07-23 13:20:00 -0500

    Delete Time.py

--- Time.py
+++
@@ -1,6 +0,0 @@
-# Import datetime class from datetime module
-import datetime
-# Use the now() attridbute on the datetime to get present time
-now=datetime.datetime.now()
-# print present time
-print("the time right now is",now)
